51328f4383a9d78220f50b2db61d8e32abc862e9
Sean.Xiao <jxzsxsp@qq.com>
2024-03-15 14:25:05 +0800

    change fpga module from veriilog1995 to verilog 2001

--- C910_RTL_FACTORY/gen_rtl/fpga/rtl/ct_f_spsram_1024x92.sv
+++ C910_RTL_FACTORY/gen_rtl/fpga/rtl/ct_f_spsram_1024x92.sv
@@ -1,11 +1,11 @@
 /*Copyright 2019-2021 T-Head Semiconductor Co., Ltd.
-
+ 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
+ 
     http://www.apache.org/licenses/LICENSE-2.0
-
+ 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,130 +13,129 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// &ModuleBeg; @3
-module ct_f_spsram_1024x92(
-  A,
-  CEN,
-  CLK,
-  D,
-  GWEN,
-  Q,
-  WEN
-);
+module ct_f_spsram_1024x92 #(
+        parameter       ADDR_WIDTH          = 10,
+        parameter       DATA_WIDTH          = 92,
+        parameter       WRAP_SIZE           = 23
+    )
+    (
+        input           [ADDR_WIDTH-1:0]    A,
+        input                               CEN,
+        input                               CLK,
+        input           [DATA_WIDTH-1:0]    D,
+        input                               GWEN,
+        output          [DATA_WIDTH-1:0]    Q,
+        input           [DATA_WIDTH-1:0]    WEN
+    );
 
-// &Ports; @4
-input   [9 :0]  A;           
-input           CEN;         
-input           CLK;         
-input   [91:0]  D;           
-input           GWEN;        
-input   [91:0]  WEN;         
-output  [91:0]  Q;           
+    // &Regs; @5
+    reg                 [ADDR_WIDTH-1:0]    addr_holding;
 
-// &Regs; @5
-reg     [9 :0]  addr_holding; 
+    // &Wires; @6
+    wire                [ADDR_WIDTH-1:0]    addr;
+    wire                [WRAP_SIZE-1:0]     ram0_din;
+    wire                [WRAP_SIZE-1:0]     ram0_dout;
+    wire                                    ram0_wen;
+    wire                [WRAP_SIZE-1:0]     ram1_din;
+    wire                [WRAP_SIZE-1:0]     ram1_dout;
+    wire                                    ram1_wen;
+    wire                [WRAP_SIZE-1:0]     ram2_din;
+    wire                [WRAP_SIZE-1:0]     ram2_dout;
+    wire                                    ram2_wen;
+    wire                [WRAP_SIZE-1:0]     ram3_din;
+    wire                [WRAP_SIZE-1:0]     ram3_dout;
+    wire                                    ram3_wen;
 
-// &Wires; @6
-wire    [9 :0]  A;           
-wire            CEN;         
-wire            CLK;         
-wire    [91:0]  D;           
-wire            GWEN;        
-wire    [91:0]  Q;           
-wire    [91:0]  WEN;         
-wire    [9 :0]  addr;        
-wire    [22:0]  ram_din0;    
-wire    [22:0]  ram_din1;    
-wire    [22:0]  ram_din2;    
-wire    [22:0]  ram_din3;    
-wire    [22:0]  ram_dout0;   
-wire    [22:0]  ram_dout1;   
-wire    [22:0]  ram_dout2;   
-wire    [22:0]  ram_dout3;   
-wire            ram_wen0;    
-wire            ram_wen1;    
-wire            ram_wen2;    
-wire            ram_wen3;    
+    // &Force("bus","Q",DATA_WIDTH-1,0); @7
 
-// &Force("bus","Q",91,0); @7
+    //write enable
+    // &Force("nonport","ram0_wen"); @13
+    // &Force("nonport","ram1_wen"); @14
+    // &Force("nonport","ram2_wen"); @15
+    // &Force("nonport","ram3_wen"); @16
 
-parameter ADDR_WIDTH = 10;
-parameter WRAP_SIZE  = 23;
+    // &Force("bus","WEN",DATA_WIDTH-1,0); @18
+    assign ram0_wen = !CEN && !WEN[22] && !GWEN;
+    assign ram1_wen = !CEN && !WEN[45] && !GWEN;
+    assign ram2_wen = !CEN && !WEN[68] && !GWEN;
+    assign ram3_wen = !CEN && !WEN[DATA_WIDTH-1] && !GWEN;
 
-//write enable
-// &Force("nonport","ram_wen0"); @13
-// &Force("nonport","ram_wen1"); @14
-// &Force("nonport","ram_wen2"); @15
-// &Force("nonport","ram_wen3"); @16
+    //din
+    // &Force("nonport","ram0_din"); @25
+    // &Force("nonport","ram1_din"); @26
+    // &Force("nonport","ram2_din"); @27
+    // &Force("nonport","ram3_din"); @28
+    // &Force("bus","D",4*WRAP_SIZE-1,0); @29
+    assign ram0_din[WRAP_SIZE-1:0] = D[WRAP_SIZE-1:0];
+    assign ram1_din[WRAP_SIZE-1:0] = D[2*WRAP_SIZE-1:WRAP_SIZE];
+    assign ram2_din[WRAP_SIZE-1:0] = D[3*WRAP_SIZE-1:2*WRAP_SIZE];
+    assign ram3_din[WRAP_SIZE-1:0] = D[4*WRAP_SIZE-1:3*WRAP_SIZE];
+    //address
+    // &Force("nonport","addr"); @35
+    always @ (posedge CLK) begin
+        if (!CEN) begin
+            addr_holding[ADDR_WIDTH-1:0] <= A[ADDR_WIDTH-1:0];
+        end
+    end
 
-// &Force("bus","WEN",91,0); @18
-assign ram_wen0 = !CEN && !WEN[22] && !GWEN;
-assign ram_wen1 = !CEN && !WEN[45] && !GWEN;
-assign ram_wen2 = !CEN && !WEN[68] && !GWEN;
-assign ram_wen3 = !CEN && !WEN[91] && !GWEN;
+    assign addr[ADDR_WIDTH-1:0] = CEN ? addr_holding[ADDR_WIDTH-1:0] : A[ADDR_WIDTH-1:0];
 
-//din
-// &Force("nonport","ram_din0"); @25
-// &Force("nonport","ram_din1"); @26
-// &Force("nonport","ram_din2"); @27
-// &Force("nonport","ram_din3"); @28
-// &Force("bus","D",4*WRAP_SIZE-1,0); @29
-assign ram_din0[WRAP_SIZE-1:0] = D[WRAP_SIZE-1:0];
-assign ram_din1[WRAP_SIZE-1:0] = D[2*WRAP_SIZE-1:WRAP_SIZE];
-assign ram_din2[WRAP_SIZE-1:0] = D[3*WRAP_SIZE-1:2*WRAP_SIZE];
-assign ram_din3[WRAP_SIZE-1:0] = D[4*WRAP_SIZE-1:3*WRAP_SIZE];
-//address
-// &Force("nonport","addr"); @35
-always@(posedge CLK)
-begin
-  if(!CEN) begin
-    addr_holding[ADDR_WIDTH-1:0] <= A[ADDR_WIDTH-1:0];
-  end
-end
+    //dout
+    // &Force("nonport","ram0_dout"); @47
+    // &Force("nonport","ram1_dout"); @48
+    // &Force("nonport","ram2_dout"); @49
+    // &Force("nonport","ram3_dout"); @50
+    assign Q[WRAP_SIZE-1:0]                = ram0_dout[WRAP_SIZE-1:0];
+    assign Q[2*WRAP_SIZE-1:WRAP_SIZE]      = ram1_dout[WRAP_SIZE-1:0];
+    assign Q[3*WRAP_SIZE-1:2*WRAP_SIZE]    = ram2_dout[WRAP_SIZE-1:0];
+    assign Q[4*WRAP_SIZE-1:3*WRAP_SIZE]    = ram3_dout[WRAP_SIZE-1:0];
 
-assign addr[ADDR_WIDTH-1:0] = CEN ? addr_holding[ADDR_WIDTH-1:0]
-                                  : A[ADDR_WIDTH-1:0];
+    fpga_ram    #(
+                    .DATAWIDTH          (WRAP_SIZE),
+                    .ADDRWIDTH          (ADDR_WIDTH)
+                )
+                ram0 (
+                    .PortAClk           (CLK),
+                    .PortAAddr          (addr),
+                    .PortADataIn        (ram0_din),
+                    .PortAWriteEnable   (ram0_wen),
+                    .PortADataOut       (ram0_dout)
+                );
 
-//dout
-// &Force("nonport","ram_dout0"); @47
-// &Force("nonport","ram_dout1"); @48
-// &Force("nonport","ram_dout2"); @49
-// &Force("nonport","ram_dout3"); @50
-assign Q[WRAP_SIZE-1:0]                = ram_dout0[WRAP_SIZE-1:0];
-assign Q[2*WRAP_SIZE-1:WRAP_SIZE]      = ram_dout1[WRAP_SIZE-1:0];
-assign Q[3*WRAP_SIZE-1:2*WRAP_SIZE]    = ram_dout2[WRAP_SIZE-1:0];
-assign Q[4*WRAP_SIZE-1:3*WRAP_SIZE]    = ram_dout3[WRAP_SIZE-1:0];
+    fpga_ram    #(
+                    .DATAWIDTH          (WRAP_SIZE),
+                    .ADDRWIDTH          (ADDR_WIDTH)
+                )
+                ram1 (
+                    .PortAClk           (CLK),
+                    .PortAAddr          (addr),
+                    .PortADataIn        (ram1_din),
+                    .PortAWriteEnable   (ram1_wen),
+                    .PortADataOut       (ram1_dout)
+                );
 
-fpga_ram #(WRAP_SIZE,ADDR_WIDTH) ram0(
-  .PortAClk (CLK),
-  .PortAAddr(addr),
-  .PortADataIn (ram_din0),
-  .PortAWriteEnable(ram_wen0),
-  .PortADataOut(ram_dout0));
+    fpga_ram    #(
+                    .DATAWIDTH          (WRAP_SIZE),
+                    .ADDRWIDTH          (ADDR_WIDTH)
+                )
+                ram2 (
+                    .PortAClk           (CLK),
+                    .PortAAddr          (addr),
+                    .PortADataIn        (ram2_din),
+                    .PortAWriteEnable   (ram2_wen),
+                    .PortADataOut       (ram2_dout)
+                );
 
-fpga_ram #(WRAP_SIZE,ADDR_WIDTH) ram1(
-  .PortAClk (CLK),
-  .PortAAddr(addr),
-  .PortADataIn (ram_din1),
-  .PortAWriteEnable(ram_wen1),
-  .PortADataOut(ram_dout1));
+    fpga_ram    #(
+                    .DATAWIDTH          (WRAP_SIZE),
+                    .ADDRWIDTH          (ADDR_WIDTH)
+                )
+                ram3 (
+                    .PortAClk           (CLK),
+                    .PortAAddr          (addr),
+                    .PortADataIn        (ram3_din),
+                    .PortAWriteEnable   (ram3_wen),
+                    .PortADataOut       (ram3_dout)
+                );
 
-fpga_ram #(WRAP_SIZE,ADDR_WIDTH) ram2(
-  .PortAClk (CLK),
-  .PortAAddr(addr),
-  .PortADataIn (ram_din2),
-  .PortAWriteEnable(ram_wen2),
-  .PortADataOut(ram_dout2));
-
-fpga_ram #(WRAP_SIZE,ADDR_WIDTH) ram3(
-  .PortAClk (CLK),
-  .PortAAddr(addr),
-  .PortADataIn (ram_din3),
-  .PortAWriteEnable(ram_wen3),
-  .PortADataOut(ram_dout3));
-
-// &ModuleEnd; @84
 endmodule
-
-
-
--- C910_RTL_FACTORY/gen_rtl/fpga/rtl/ct_f_spsram_2048x128.sv
+++ C910_RTL_FACTORY/gen_rtl/fpga/rtl/ct_f_spsram_2048x128.sv
@@ -1,11 +1,11 @@
 /*Copyright 2019-2021 T-Head Semiconductor Co., Ltd.
-
+ 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
+ 
     http://www.apache.org/licenses/LICENSE-2.0
-
+ 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,78 +13,61 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// &Depend("fpga_ram.v"); @23
+module ct_f_spsram_2048x128 #(
+        parameter       ADDR_WIDTH          = 11,
+        parameter       DATA_WIDTH          = 128,
+        parameter       WRAP_SIZE           = 128
+    )
+    (
+        input           [ADDR_WIDTH-1:0]    A,
+        input                               CEN,
+        input                               CLK,
+        input           [DATA_WIDTH-1:0]    D,
+        input                               GWEN,
+        output          [DATA_WIDTH-1:0]    Q,
+        input           [DATA_WIDTH-1:0]    WEN
+    );
 
-// &ModuleBeg; @25
-module ct_f_spsram_2048x128(
-  A,
-  CEN,
-  CLK,
-  D,
-  GWEN,
-  Q,
-  WEN
-);
+    // &Regs; @27
+    reg                 [ADDR_WIDTH-1:0]    addr_holding;
 
-// &Ports; @26
-input   [10 :0]  A;           
-input            CEN;         
-input            CLK;         
-input   [127:0]  D;           
-input            GWEN;        
-input   [127:0]  WEN;         
-output  [127:0]  Q;           
+    // &Wires; @28
+    wire                [ADDR_WIDTH-1:0]    addr;
+    wire                [WRAP_SIZE-1:0]     ram0_din;
+    wire                [WRAP_SIZE-1:0]     ram0_dout;
+    wire                                    ram0_wen;
 
-// &Regs; @27
-reg     [10 :0]  addr_holding; 
+    //write enable
+    // &Force("nonport","ram0_wen"); @34
+    // &Force("bus","WEN",DATA_WIDTH-1,0); @35
+    assign ram0_wen = !CEN && !WEN[DATA_WIDTH-1] && !GWEN;
+    //din
+    // &Force("nonport","ram0_din"); @38
+    // &Force("bus","D",WRAP_SIZE-1,0); @39
+    assign ram0_din[WRAP_SIZE-1:0] = D[WRAP_SIZE-1:0];
+    //address
+    // &Force("nonport","addr"); @42
+    always @ (posedge CLK) begin
+        if (!CEN) begin
+            addr_holding[ADDR_WIDTH-1:0] <= A[ADDR_WIDTH-1:0];
+        end
+    end
 
-// &Wires; @28
-wire    [10 :0]  A;           
-wire             CEN;         
-wire             CLK;         
-wire    [127:0]  D;           
-wire             GWEN;        
-wire    [127:0]  Q;           
-wire    [127:0]  WEN;         
-wire    [10 :0]  addr;        
-wire    [127:0]  ram_din;     
-wire    [127:0]  ram_dout;    
-wire             ram_wen;     
+    assign addr[ADDR_WIDTH-1:0] = CEN ? addr_holding[ADDR_WIDTH-1:0] : A[ADDR_WIDTH-1:0];
+    //dout
+    // &Force("nonport","ram0_dout"); @53
+    assign Q[WRAP_SIZE-1:0]                = ram0_dout[WRAP_SIZE-1:0];
 
+    fpga_ram    #(
+                    .DATAWIDTH          (WRAP_SIZE),
+                    .ADDRWIDTH          (ADDR_WIDTH)
+                )
+                ram0 (
+                    .PortAClk           (CLK),
+                    .PortAAddr          (addr),
+                    .PortADataIn        (ram0_din),
+                    .PortAWriteEnable   (ram0_wen),
+                    .PortADataOut       (ram0_dout)
+                );
 
-parameter ADDR_WIDTH = 11;
-parameter WRAP_SIZE  = 128;
-
-//write enable
-// &Force("nonport","ram_wen"); @34
-// &Force("bus","WEN",127,0); @35
-assign ram_wen = !CEN && !WEN[127] && !GWEN;
-//din
-// &Force("nonport","ram_din"); @38
-// &Force("bus","D",WRAP_SIZE-1,0); @39
-assign ram_din[WRAP_SIZE-1:0] = D[WRAP_SIZE-1:0];
-//address
-// &Force("nonport","addr"); @42
-always@(posedge CLK)
-begin
-  if(!CEN) begin
-    addr_holding[ADDR_WIDTH-1:0] <= A[ADDR_WIDTH-1:0];
-  end
-end
-
-assign addr[ADDR_WIDTH-1:0] = CEN ? addr_holding[ADDR_WIDTH-1:0]
-                                  : A[ADDR_WIDTH-1:0];
-//dout
-// &Force("nonport","ram_dout"); @53
-assign Q[WRAP_SIZE-1:0]                = ram_dout[WRAP_SIZE-1:0];
-fpga_ram #(WRAP_SIZE,ADDR_WIDTH) ram(
-  .PortAClk (CLK),
-  .PortAAddr(addr),
-  .PortADataIn (ram_din),
-  .PortAWriteEnable(ram_wen),
-  .PortADataOut(ram_dout));
-
-// &ModuleEnd; @62
 endmodule
-
-
