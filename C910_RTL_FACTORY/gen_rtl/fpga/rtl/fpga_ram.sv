ea295cd5f739ce7834ecd1f1cd2f6b126ad8c6b1
Sean.Xiao <jxzsxsp@qq.com>
2024-03-21 14:18:49 +0800

    change ifu module from veriilog1995 to verilog 2001

--- C910_RTL_FACTORY/gen_rtl/fpga/rtl/fpga_ram.sv
+++ C910_RTL_FACTORY/gen_rtl/fpga/rtl/fpga_ram.sv
@@ -22,13 +22,12 @@ module fpga_ram #(
         input           [(DATAWIDTH-1):0]   PortADataIn,
         input                               PortAWriteEnable,
         //input                             PortAChipEnable,
-        output          [(DATAWIDTH-1):0]   PortADataOut
+        output reg      [(DATAWIDTH-1):0]   PortADataOut
     );
 
     localparam          MEMDEPTH            = 2**(ADDRWIDTH);
 
     reg                 [(DATAWIDTH-1):0]   mem [(MEMDEPTH-1):0] /* synthesis syn_ramstyle = "no_rw_check" */;
-    reg                 [(DATAWIDTH-1):0]   PortADataOut;
 
     always @ (posedge PortAClk) begin
         if (PortAWriteEnable) begin
